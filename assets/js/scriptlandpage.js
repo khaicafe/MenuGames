e4d149f4bfd31c3e43b2a37f94e5f57616bc76c6
khaicafe2021 <87179311+khaicafe2021@users.noreply.github.com>
2022-10-12 10:08:57 +0700

    Update scriptlandpage.js

--- assets/js/scriptlandpage.js
+++ assets/js/scriptlandpage.js
@@ -109,7 +109,7 @@ async function connection (socket, timeout = 10000) {
         return response.json(); // parses JSON response into native JavaScript objects
       } else {
           // Dang nhap lai
-          window.location.href="/";
+        //   window.location.href="/";
       }
       
     }
